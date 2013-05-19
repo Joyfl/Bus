d97e7ff7e1091b3af6d3ac65feb583b61905ecc1
theeluwin <theeluwin@gmail.com>
2013-05-20 08:49:58 +0900

    migrating to new server

--- bus/settings.py
+++ bus/settings.py
@@ -45,7 +45,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/bus/media/'
+MEDIA_ROOT = '/home/bus/bus/Bus/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/bus/static/'
+STATIC_ROOT = '/home/bus/bus/Bus/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'bus.wsgi.application'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 TEMPLATE_DIRS = (
-	'/home/bus/template/',
+	'/home/bus/bus/Bus/template/',
 )
 
 INSTALLED_APPS = (
